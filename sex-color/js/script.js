d8481789c2480f5f597d45937a5d938546dfa4fd
Mr2803 <rispoli.mar2803@gmail.com>
2019-11-04 16:58:42 +0100

    fix script

--- sex-color/js/script.js
+++ sex-color/js/script.js
@@ -1,38 +1,39 @@
 /*  2.  Il software deve chiedere all’utente il suo nome e il sesso con due prompt diversi.
 Sulla pagina html deve apparire “Ciao <nome>“, il colore del nome deve essere azzurro o rosa a seconda del sesso inserito. */
 //@ts-check
-
-var nome;
 var sesso;
+var nome;
 var el;
-var elementi = [];
-var valoreValido = false;
-var valoreValido2 = false;
+
+
 
 el = document.getElementById("saluto");
 
 
 
-function chiediNome(name){
-    valoreValido;
+function chiediNome(){
+    var valoreValido = false;
+    var name;
+  
     while(valoreValido == false){
         name = prompt("Inserisci il tuo nome");
         if(!isNaN(name)){
             alert("Inserisci un valore corretto")
         }else{
             valoreValido = true;
-            elementi.push(name);
         }
     }
+    return name;
 }
 
-function chiediSesso(sex,name){
-    valoreValido2;
-    while(valoreValido2 == false){
+function chiediSesso(name){
+    var valoreValido = false;
+    var sex; 
+
+    while(valoreValido == false){
         sex = prompt("Inserisci il tuo sesso");
         if (sex == "uomo" || sex == "m" || sex == "M" || sex == "donna" || sex == "f" || sex == "F"){
-            elementi.push(sex);
-            valoreValido2 = true;
+            valoreValido = true;
             if (sex == "uomo" || sex == "m" || sex == "M") {
                 el.style.color = "blue";
                 el.innerHTML = "Ciao " + name;
@@ -44,11 +45,13 @@ function chiediSesso(sex,name){
             alert("inserisci un valore valido")
         }
     }
+    return sex;
 }
 
-while (elementi.length < 2) {
-    chiediNome(nome);
-    chiediSesso(sesso,nome);
-}
 
-console.log(elementi)
+    nome = chiediNome();
+    console.log(nome)//Chiara
+    sesso = chiediSesso(nome);
+
+
+//console.log(elementi)
