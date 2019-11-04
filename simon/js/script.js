96a6158fca9082505e7b8ce4c89d20a1c0173e4d
Mr2803 <rispoli.mar2803@gmail.com>
2019-11-04 13:33:47 +0100

    finish structure of script (only the timer is missing)

---
+++ simon/js/script.js
@@ -0,0 +1,46 @@
+/* Un alert espone 5 numeri casuali.Da li parte un timer di 30s.
+Dopo 30 secondi l utente deve inserire un prompt alla volta i numeri che ha visto precedent.Dopo che sono stati inseriti i 5 numeri , il software dice quanti e quali dei numeri da indovinare sono stati individuati */
+//@ts-check
+
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min)) + min; // il max è escluso il min è incluso
+}
+
+var gruppoNumDaRicordare = [];
+console.log("questi sono i numeri da ricordare" + gruppoNumDaRicordare)
+var numRand;
+
+while (gruppoNumDaRicordare.length < 5) {
+    numRand = getRandomInt(1, 101);
+    if (gruppoNumDaRicordare.includes(numRand) == false) {
+        gruppoNumDaRicordare.push(numRand);
+    }
+
+}
+console.log(gruppoNumDaRicordare)
+alert("Guarda questi numeri e ricordali , quando sei pronto premi ok e aspetta 30 secondi " + gruppoNumDaRicordare)
+
+var avviso = alert("inserisci i numeri che hai visto prima , se li ricordi");
+setTimeout("insertNum", 3000)
+
+var gruppoNumeriGiocatore = [];
+var punteggio = 0;
+var numeriIndovinati = [];
+
+//Fatto con il for con il while avrei semplicemente messo la condizione i<gruppoNumDaRicordare.length 
+for (var i = 0; i < gruppoNumDaRicordare.length; i++) {
+    var insertNum = parseInt(prompt("inserisci un numero"));
+
+    gruppoNumeriGiocatore.push(insertNum);
+
+    if (gruppoNumDaRicordare.includes(insertNum) == true) {
+        punteggio++;
+        numeriIndovinati.push(insertNum);
+    }
+}
+
+console.log("questi sono i numeri del giocatore " + gruppoNumeriGiocatore)
+console.log("questo è il punteggio che hai totalizzato :" + punteggio)
+console.log("questi sono i numeri che hai indovinato : " + numeriIndovinati)
