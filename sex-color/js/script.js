fbcb4d36e535941d6ed2af0ab6530371c2726746
Mr2803 <rispoli.mar2803@gmail.com>
2019-11-04 14:43:23 +0100

    add controls in sex-color

---
+++ sex-color/js/script.js
@@ -0,0 +1,34 @@
+/*  2.  Il software deve chiedere all’utente il suo nome e il sesso con due prompt diversi.
+Sulla pagina html deve apparire “Ciao <nome>“, il colore del nome deve essere azzurro o rosa a seconda del sesso inserito. */
+//@ts-check
+
+var nome;
+var sesso;
+var el;
+var elementi = [];
+
+
+el = document.getElementById("saluto");
+
+while(elementi.length < 2){
+    nome = prompt("Inserisci il tuo nome");
+    sesso = prompt("Inserisci il tuo sesso");
+    if (sesso == "uomo" || sesso == "m" || sesso == "M" || sesso == "donna" || sesso == "f" || sesso == "F"){
+        if (sesso == "uomo" || sesso == "m" || sesso == "M") {
+            elementi.push(nome);
+            elementi.push(sesso);
+            el.style.color = "blue"
+            el.innerHTML = "Ciao " + nome;
+        } else if (sesso == "donna" || sesso == "f" || sesso == "F") {
+            elementi.push(nome);
+            elementi.push(sesso);
+            el.style.color = "pink"
+            el.innerHTML = "Ciao " + nome;
+            }
+        } else {
+            alert("inserisci un valore valido per il sesso");
+        }
+
+}
+
+console.log(elementi)
